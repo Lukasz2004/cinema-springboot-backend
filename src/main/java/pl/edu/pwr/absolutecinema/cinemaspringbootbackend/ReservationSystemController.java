16f059900f9eff6042a761403fc8b8d9a6c3faf4
Lukasz2004 <lukaszczerwinski2004@gmail.com>
2025-12-08 01:27:09 +0100

    First test APIs

--- src/main/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/ReservationSystemController.java
+++ src/main/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/ReservationSystemController.java
@@ -1,4 +1,29 @@
 package pl.edu.pwr.absolutecinema.cinemaspringbootbackend;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class ReservationSystemController {
+
+    private final JdbcTemplate jdbcTemplate;
+    public ReservationSystemController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @GetMapping("/oracle-test")
+    public List<Map<String, Object>> getOracleData() {
+        String sql = "SELECT * FROM FILM FETCH FIRST 10 ROWS ONLY";
+        return jdbcTemplate.queryForList(sql);
+    }
+    @GetMapping("/przekaski")
+    public List<Map<String, Object>> listaPrzekasek() {
+        String sql = "SELECT * FROM PRODUKT ORDER BY PRODUKT.TYPPRODUKTU";
+        return jdbcTemplate.queryForList(sql);
+    }
 }
--- src/main/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/TestController.java
+++ src/main/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/TestController.java
@@ -1,4 +1,31 @@
 package pl.edu.pwr.absolutecinema.cinemaspringbootbackend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    private final String LOREM_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+            "nisi ut aliquip ex ea commodo consequat.";
+
+    @GetMapping("/test/helloWorld")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+    @GetMapping("/test/loremIpsum")
+    public String lorem(@RequestParam int iloscZnakow) {
+        if (iloscZnakow > LOREM_TEXT.length()) {
+            return LOREM_TEXT;
+        }
+
+        if (iloscZnakow < 0) {
+            return "Błąd: Ilość znaków nie może być ujemna!";
+        }
+
+        return LOREM_TEXT.substring(0, iloscZnakow);
+
+    }
 }
