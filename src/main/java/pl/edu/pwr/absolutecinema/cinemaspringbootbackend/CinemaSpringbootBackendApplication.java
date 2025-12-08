5bf54b28f514b894ad0cf5eb5d47bbfd5cc92713
Lukasz2004 <lukaszczerwinski2004@gmail.com>
2025-12-08 01:25:35 +0100

    Initial commit

---
+++ src/main/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/CinemaSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.absolutecinema.cinemaspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSpringbootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/CinemaSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.absolutecinema.cinemaspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
