356ae63995007240ebc0a3891be01dd178cbd686
Lukasz2004 <lukaszczerwinski2004@gmail.com>
2026-01-03 23:11:32 +0100

    Legacy APIs removal

--- src/main/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/SaleSystemController.java
+++ src/main/java/pl/edu/pwr/absolutecinema/cinemaspringbootbackend/SaleSystemController.java
@@ -42,12 +42,6 @@ public class SaleSystemController {
     @Operation(summary = "Zwraca dane o bilecie wskazanym po id parametrem idBiletu" , description = "Zwraca wszystkie dane widoku v_pokazBilet. \n\n" + securityNoticeString)
     public List<Map<String, Object>> getBilet(@Parameter(description = "idBiletu do zwrócenia")@PathVariable int idBiletu, @AuthenticationPrincipal UserDetails userDetails) {
         authService.verifyStaff(userDetails);
-        return getBiletNoAuth(idBiletu);
-    }
-    @GetMapping("/legacy/getBilet/{idBiletu}")
-    @Operation(summary = "Jak getBilet ale bez autoryzacji", description = "Dokładny opis w getBilet")
-    @SecurityRequirements //Info dla Swaggera: Nie wymaga Tokena
-    public List<Map<String, Object>> getBiletNoAuth(@PathVariable int idBiletu) {
         String sql = "SELECT * FROM v_PokazBIlet WHERE biletID = ?";
         return jdbcTemplate.queryForList(sql, idBiletu);
     }
@@ -56,13 +50,6 @@ public class SaleSystemController {
     @Operation(summary = "Ustawia status biletu o wskazanym po id parametrem idBiletu na \"Wykorzystany\"" , description = securityNoticeString)
     public void useBilet(@Parameter(description = "idBiletu do zużycia")@PathVariable int idBiletu, @AuthenticationPrincipal UserDetails userDetails) {
         authService.verifyStaff(userDetails);
-        useBiletNoAuth(idBiletu);
-        return;
-    }
-    @PatchMapping("/legacy/useBilet/{idBiletu}")
-    @Operation(summary = "Jak checkBilet ale bez autoryzacji", description = "Dokładny opis w useBilet")
-    @SecurityRequirements //Info dla Swaggera: Nie wymaga Tokena
-    public void useBiletNoAuth(@PathVariable int idBiletu) {
         String sql = "UPDATE Bilet SET status = 'Wykorzystany' WHERE biletID = ?";
         jdbcTemplate.update(sql, idBiletu);
         return;
@@ -72,12 +59,6 @@ public class SaleSystemController {
     @Operation(summary = "Zwraca pełen repertuar kina" , description = "Zwraca wszystkie filmy w kinie posiadające aktualne seanse.\n\n Dodatkowo dołącza: wszystkie możliwe języki seansów które się odbywają\n\n" + securityNoticeString)
     public List<Map<String, Object>> getMovies(@AuthenticationPrincipal UserDetails userDetails) {
         authService.verifyStaff(userDetails);
-        return repertuarService.getRepertuarShort();
-    }
-    @GetMapping("/legacy/getMovies")
-    @Operation(summary = "Jak getMovies ale bez autoryzacji", description = "Dokładny opis w getMovies")
-    @SecurityRequirements //Info dla Swaggera: Nie wymaga Tokena
-    public List<Map<String, Object>> getMoviesNoAuth(@AuthenticationPrincipal UserDetails userDetails) {
         return repertuarService.getRepertuarShort();
     }
 
